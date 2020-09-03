8c546053e4a62d051352b443da1db4a1edd7e62a
James Backhouse <jwbackhouse@gmail.com>
2020-09-03 22:35:26 +0100

    Create variable for directory name.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // host, user, password, database
 var { cnxDetails } = require('./connection.js')
 
+// Set directory name here
+const FOLDER = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads"
+
 // Establish mySQL connection
 var mysql = require('mysql');
 var connection = mysql.createConnection({ cnxDetails });
@@ -9,27 +12,27 @@ var connection = mysql.createConnection({ cnxDetails });
 // Pull file names from directory in series
 var fs = require('fs');
 var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var i = 0;
-    (function next() {
-      var file = list[i++];
-      if (!file) return done(null, results);
-      file = dir + '/' + file;
-      fs.stat(file, function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            next();
-          });
-        } else {
-          results.push(file);
-          next();
-        }
-      });
-    })();
-  });
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file) return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function(err, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    results.push(file);
+                    next();
+                }
+            });
+        })();
+    });
 };
 
 // Parallel version of the above
@@ -59,33 +62,31 @@ var walk = function(dir, done) {
 // };
 
 // Log results as array
-walk("C:/ProgramData/MySQL/MySQL Server 8.0/Uploads", function(err, results) {
-  if (err) throw err;
-  console.log(results);
+walk(DIRECTORY, function(err, results) {
+    if (err) throw err;
+    console.log(results);
 
-  // Extract filenames
-  results.forEach(function(element) {
+    // Extract filenames
+    results.forEach(function(element) {
+        // Set escaped constants
+        const enclosed = '\"';
+        const terminate = '\\n';
 
-    // Check SQL connection
-    // connection.connect(function(err) {
-    //   if (err) throw err;
-    //   console.log("Connected!");
-    // });
+        // Check SQL connection
+        // connection.connect(function(err) {
+        //   if (err) throw err;
+        //   console.log("Connected!");
+        // });
 
-    // Set escaped constants
-    const enclosed = '\"';
-    const terminate = '\\n';
-
-    // Run SQl query
-    connection.query("SET SESSION sql_mode = ''");
-    connection.query("LOAD DATA INFILE '" + element + "' INTO TABLE darksky CHARACTER SET latin1 FIELDS OPTIONALLY ENCLOSED BY '" + enclosed + "' TERMINATED BY ',' LINES TERMINATED BY '" + terminate + "' IGNORE 1 ROWS (`latitude`,`longitude`,`day.time`,`day.summary`,`day.icon`,`day.sunriseTime`,`day.sunsetTime`,`day.moonPhase`,`day.precipIntensity`,`day.precipIntensityMax`,`day.precipProbability`,`day.temperatureHigh`,@dummy,`day.temperatureLow`,@dummy,`day.apparentTemperatureHigh`,`day.apparentTemperatureHighTime`,`day.apparentTemperatureLow`,`day.apparentTemperatureLowTime`,@dummy,@dummy,@dummy,`day.windSpeed`,`day.windGust`,@dummy,@dummy,`day.cloudCover`,`day.uvIndex`,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,`hour.time`,`hour.summary`,`hour.icon`,`hour.precipIntensity`,`hour.precipProbability`,`hour.temperature`,`hour.apparentTemperature`,@dummy,@dummy,@dummy,`hour.windSpeed`,`hour.windGust`,@dummy,`hour.cloudCover`,`hour.uvIndex`,@dummy) SET `day.time` = from_unixtime(@daytime, '%Y-%m-%d'), `day.sunriseTime` = from_unixtime(@sunriseTime, '%Y-%m-%d %T'), `day.sunsetTime` = from_unixtime(@sunsetTime, '%Y-%m-%d %T'), `day.apparentTemperatureHighTime` = from_unixtime(@dayapparentTemperatureHighTime, '%Y-%m-%d %T'), `day.apparentTemperatureLowTime` = from_unixtime(@dayapparentTemperatureLowTime, '%Y-%m-%d %T')",
-    function (error, results) {
-      if (error) console.error(element + error);
-      // console.log(results);
+        // Run SQl query
+        connection.query("SET SESSION sql_mode = ''");
+        connection.query("LOAD DATA INFILE '" + element + "' INTO TABLE darksky CHARACTER SET latin1 FIELDS OPTIONALLY ENCLOSED BY '" + enclosed + "' TERMINATED BY ',' LINES TERMINATED BY '" + terminate + "' IGNORE 1 ROWS (`latitude`,`longitude`,`day.time`,`day.summary`,`day.icon`,`day.sunriseTime`,`day.sunsetTime`,`day.moonPhase`,`day.precipIntensity`,`day.precipIntensityMax`,`day.precipProbability`,`day.temperatureHigh`,@dummy,`day.temperatureLow`,@dummy,`day.apparentTemperatureHigh`,`day.apparentTemperatureHighTime`,`day.apparentTemperatureLow`,`day.apparentTemperatureLowTime`,@dummy,@dummy,@dummy,`day.windSpeed`,`day.windGust`,@dummy,@dummy,`day.cloudCover`,`day.uvIndex`,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,`hour.time`,`hour.summary`,`hour.icon`,`hour.precipIntensity`,`hour.precipProbability`,`hour.temperature`,`hour.apparentTemperature`,@dummy,@dummy,@dummy,`hour.windSpeed`,`hour.windGust`,@dummy,`hour.cloudCover`,`hour.uvIndex`,@dummy) SET `day.time` = from_unixtime(@daytime, '%Y-%m-%d'), `day.sunriseTime` = from_unixtime(@sunriseTime, '%Y-%m-%d %T'), `day.sunsetTime` = from_unixtime(@sunsetTime, '%Y-%m-%d %T'), `day.apparentTemperatureHighTime` = from_unixtime(@dayapparentTemperatureHighTime, '%Y-%m-%d %T'), `day.apparentTemperatureLowTime` = from_unixtime(@dayapparentTemperatureLowTime, '%Y-%m-%d %T')",
+        function (error, results) {
+            if (error) console.error(element + error);
+            // console.log(results);
+        });
     });
 
-  });
-
-  // Close connection
-  connection.end();
+    // Close connection
+    connection.end();
 });
